1f1e9d5e0856cbe148ac2c3ad7ac80ddf4e7f7c3
Valantis Alatsas <valantis.alatsas@gmail.com>
2021-09-21 20:35:09 +0300

    Add project files.

---
+++ timebreaker/FormA.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace timebreaker
+{
+    partial class FormA
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormA));
+            this.TimeBreaker = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // TimeBreaker
+            // 
+            this.TimeBreaker.Icon = ((System.Drawing.Icon)(resources.GetObject("TimeBreaker.Icon")));
+            this.TimeBreaker.Text = "Time Breaker";
+            this.TimeBreaker.Visible = true;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 5000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // FormA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 0);
+            this.MinimizeBox = false;
+            this.Name = "FormA";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "FormA";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon TimeBreaker;
+        private System.Windows.Forms.Timer timer;
+    }
+}
---
+++ timebreaker/FormA.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace timebreaker
+{
+    public partial class FormA : Form
+    {
+        public FormA()
+        {
+            InitializeComponent();
+            timer.Interval = int.MaxValue;
+            TimeBreaker.ContextMenuStrip = new ContextMenuStrip();
+            TimeBreaker.ContextMenuStrip.Items.Add("10 Seconds", null);
+            TimeBreaker.ContextMenuStrip.Items.Add("10 Minutes", null);
+            TimeBreaker.ContextMenuStrip.Items.Add("30 Minutes", null);
+            TimeBreaker.ContextMenuStrip.Items.Add("1 Hour", null);
+            TimeBreaker.ContextMenuStrip.Items.Add("2 Hours", null);
+            TimeBreaker.ContextMenuStrip.Items.Add("3 Hours", null);
+            TimeBreaker.ContextMenuStrip.Items.Add("6 Hours", null);
+            TimeBreaker.ContextMenuStrip.Items.Add("Stop", null);
+            TimeBreaker.ContextMenuStrip.ItemClicked += new ToolStripItemClickedEventHandler(ToolStrip_Clicked);
+        }
+
+        private void ToolStrip_Clicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Text)
+            {
+                case "10 Seconds":
+                    timer.Interval = 10000;
+                    break;
+                case "10 Minutes":
+                    timer.Interval = 600000;
+                    break;
+                case "30 Minutes":
+                    timer.Interval = 1800000;
+                    break;
+                case "1 Hour":
+                    timer.Interval = 3600000;
+                    break;
+                case "2 Hours":
+                    timer.Interval = 7200000;
+                    break;
+                case "3 Hours":
+                    timer.Interval = 10800000;
+                    break;
+                case "6 hours":
+                    timer.Interval = 21600000;
+                    break;
+            }
+            timer.Stop();
+            if (e.ClickedItem.Text != "Stop")
+            {
+                timer.Start();
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (timer.Interval != 300000)
+            {
+                TimeBreaker.ShowBalloonTip(0, "Break Time", "Take a 5 minute break", ToolTipIcon.Warning);
+            }
+            else
+            {
+                TimeBreaker.ShowBalloonTip(0, "Break Over", "Back to work", ToolTipIcon.Info);
+            }
+            timer.Interval = 300000;
+        }
+    }
+}
